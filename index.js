803d6dc41f1c13418f56f88bdd7d12ad8c7bb6f1
Aleksandar Brajkovic <aspireledd@gmail.com>
2019-12-03 22:13:01 +0100

    reformat

--- index.js
+++ index.js
@@ -1,82 +1,117 @@
-const run = ({
-    svg,
-    d,
-    markerOptions
-}) => {
+(() => {
+    const setAttributesFromOptions = (d, dashed, markerOptions, marker) => {
+        const [M] = d.split(/(?=[LMC])/);
+        dashed.setAttribute('d', M);
 
-    const marker = document.createElementNS('http://www.w3.org/2000/svg', markerOptions.head.elem);
+        Object.keys(markerOptions.tail).forEach((attr) => {
+            dashed.setAttribute(attr, markerOptions.tail[attr]);
+        });
 
-    const dashed = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    const [M] = d.split(/(?=[LMC])/);
-
-    dashed.setAttribute('d', M);
-
-    Object.keys(markerOptions.tail).forEach((attr) => {
-        dashed.setAttribute(attr, markerOptions.tail[attr]);
-    });
-
-    Object.keys(markerOptions.head).forEach((attr) => {
-        marker.setAttribute(attr, markerOptions.head[attr]);
-    });
-
-    const context = document.querySelector(svg);
-
-    context.appendChild(marker);
-    context.appendChild(dashed);
-
-
-    const clone = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    clone.setAttribute('d', d);
-
-    const totalLength = clone.getTotalLength();
-    let idx = 0;
-    const speed = markerOptions.speed || 2;
-    const run = () => {
-        const {
-            x,
-            y
-        } = clone.getPointAtLength(idx);
-
-        dashed.setAttribute('d', dashed.getAttribute('d') + ' ' + "L " + x + ',' + y);
-        marker.setAttribute('cx', x);
-        marker.setAttribute('cy', y);
-
-        if (idx < totalLength) {
-            idx = idx + speed;
-            requestAnimationFrame(run);
-        }
+        Object.keys(markerOptions.head).forEach((attr) => {
+            marker.setAttribute(attr, markerOptions.head[attr]);
+        });
     };
 
-    requestAnimationFrame(run);
-}
+    const createClone = d => {
+        const clone = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        clone.setAttribute('d', d);
+        return clone;
+    };
 
-const markerTail = ({
-    svg = 'svg',
-    d,
-    markerOptions = {}
-}) => {
-    const markerDefaults = {
-        speed: 3,
-        head: {
-            elem: 'circle',
-            fill: 'black',
-            r: 2
-        },
-        tail: {
-            fill: 'none',
-            stroke: 'black',
-            'stroke-dasharray': 3
-        },
+    const running = (idx, totalLength) => idx < totalLength;
+
+    const followAlongPath = (markerOptions, clone, dashed, marker, totalLength, onEnd) => {
+        let idx = 0;
+        const speed = markerOptions.speed || 2;
+        let destroy = null;
+
+        const run = () => {
+            const {
+                x,
+                y
+            } = clone.getPointAtLength(idx);
+
+            dashed.setAttribute('d', dashed.getAttribute('d') + ' ' + "L " + x + ',' + y);
+            marker.setAttribute('cx', x);
+            marker.setAttribute('cy', y);
+
+            if (running(idx, totalLength)) {
+                idx = idx + speed;
+                destroy = requestAnimationFrame(run);
+            } else {
+                cancelAnimationFrame(destroy);
+
+                if (typeof onEnd === 'function') {
+                    onEnd({
+                        marker,
+                        dashed
+                    });
+                }
+            }
+        };
+
+        destroy = requestAnimationFrame(run);
+    };
+
+    const bootstrap = ({
+                           svg,
+                           d,
+                           markerOptions,
+                           onEnd
+                       }) => {
+
+        const marker = document.createElementNS('http://www.w3.org/2000/svg', markerOptions.head.elem);
+        const dashed = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+
+        setAttributesFromOptions(d, dashed, markerOptions, marker);
+
+        const context = document.querySelector(svg);
+
+        context.appendChild(marker);
+        context.appendChild(dashed);
+
+        const clone = createClone(d);
+        const totalLength = clone.getTotalLength();
+
+        followAlongPath(markerOptions, clone, dashed, marker, totalLength, onEnd);
+    };
+
+    const markerTail = ({
+                            svg = 'svg',
+                            d,
+                            markerOptions = {},
+                            onEnd
+                        }) => {
+        const markerDefaults = {
+            speed: 3,
+            head: {
+                elem: 'circle',
+                fill: 'black',
+                r: 2
+            },
+            tail: {
+                fill: 'none',
+                stroke: 'black',
+                'stroke-dasharray': 3
+            },
+            onEnd: () => {
+                return null;
+            }
+        };
+
+
+        bootstrap({
+            svg,
+            d,
+            markerOptions: Object.assign({}, markerDefaults, markerOptions),
+            onEnd
+        });
     };
 
 
-    const app = run({
-        svg,
-        d,
-        markerOptions: Object.assign({}, markerDefaults, markerOptions)
-    });
-};
-
-if (typeof module != "undefined" && module.exports) {
-    module.exports = markerTail;
-}
+    if (typeof module != "undefined" && module.exports) {
+        module.exports = markerTail;
+    } else {
+        window.studiousDoodle = markerTail;
+    }
+})()
